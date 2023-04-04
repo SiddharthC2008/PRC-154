ad03d7195ab752162fd2effe3eb56bdbe3dfa7d2
SiddharthC2008 <93938307+SiddharthC2008@users.noreply.github.com>
2023-04-04 14:54:41 -0400

    Add files via upload

---
+++ fishTreasure.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent("coins", {
+  init: function () {
+  for (var i=1; i <= 10; i++) {
+  const id = 'coins(1);
+  const posX = Math.random() 100-50;
+  const posY= Math.random()
+  const posz= Math.random() 68+-40;
+  const position (x: posX, y: posY, z: posZ };
+  this.createCoins(id, position);
+  }
+  },
+  createCoins: function (id, position) {
+  const treasureEntity
+  document.querySelector("#treasureCoins");
+  var coinEl
+  document.createElement("a-entity");
+  coinEl.setAttribute("id", id);
+  }); 
